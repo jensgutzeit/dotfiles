404b237aa3fd34d2858e1ef4a65dafc99e26660f
Jens Gutzeit <jens@gutzeit.name>
2014-05-10 20:50:51 +0200

    Add initial conky configuration

---
+++ conky/.conky_functions/pad.lua
@@ -0,0 +1,3 @@
+function conky_pad(number)
+   return string.format('%3s', conky_parse(number))
+end
--- install.sh
+++ install.sh
@@ -9,32 +9,36 @@ txtrst=$(tput sgr0)
 OK="${txtbld}${txtwht}[${txtgrn}OK${txtwht}]${txtrst}"
 FAIL="${txtbld}${txtwht}[${txtred}FAIL${txtwht}]${txtrst}"
 
+function show_result {
+   if [ $1 -eq 0 ]; then
+      echo $OK
+   else
+      echo $FAIL
+   fi
+}
+
 pwd=`pwd`
 
 echo "Using source directory '$pwd' ... "
 
 printf "Creating .gitconfig ... "
 ln -sf $pwd/git/.gitconfig ~/.gitconfig
-if [ $? -eq 0 ]; then
-   echo $OK
-else
-   echo $FAIL
-fi
+show_result $?
 
 printf "Creating .vimrc ... "
 ln -sf $pwd/vim/.vimrc ~/.vimrc
-if [ $? -eq 0 ]; then
-   echo $OK
-else
-   echo $FAIL
-fi
+show_result $?
 
 printf "Creating .vim ... "
 ln -sf $pwd/vim/.vim ~/.vim
-if [ $? -eq 0 ]; then
-   echo $OK
-else
-   echo $FAIL
-fi
+show_result $?
+
+printf "Creating .conky ... "
+ln -sf $pwd/conky/.conky ~/.conky
+show_result $?
+
+printf "Creating .conky_functions ... "
+ln -sf $pwd/conky/.conky_functions ~/.conky_functions
+show_result $?
 
 echo "Done"
